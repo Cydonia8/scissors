b500124a2805560c6b631dedbdc98e297fac87c3
cydonia8 <holymustaine20@gmail.com>
2022-11-10 10:54:41 +0100

    form

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 "use strict"
 const enviar = document.getElementById("enviar");
-let nombre_jugador
-enviar.addEventListener("onclick", ()=>{
+const info_nombre_jugador = document.getElementById("nombre-jugador");
+let nombre_jugador = document.getElementById("input-usu").value;;
+// function registrarNombre(){
+//     const nombre = document.getElementById("input-usu").value;
+//     nombre_jugador = nombre;
+// };
+enviar.addEventListener("click", ()=>{
     nombre_jugador = document.getElementById("input-usu").value;
-    console.log(nombre_jugador);
-})
-
+    info_nombre_jugador.innerText=nombre_jugador;
+});
+console.log(nombre_jugador);
