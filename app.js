93d57c3a3cccfb3552a6140d7257e286b8571b35
cydonia8 <holymustaine20@gmail.com>
2022-11-11 12:51:07 +0100

    s

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 "use strict"
 const enviar = document.getElementById("enviar");
 const info_nombre_jugador = document.getElementById("nombre-jugador");
-let nombre_jugador = document.getElementById("input-usu").value;;
 // function registrarNombre(){
 //     const nombre = document.getElementById("input-usu").value;
 //     nombre_jugador = nombre;
 // };
 enviar.addEventListener("click", ()=>{
-    nombre_jugador = document.getElementById("input-usu").value;
+    const nombre_jugador = document.getElementById("input-usu").value;
     info_nombre_jugador.innerText=nombre_jugador;
 });
-console.log(nombre_jugador);
+
+
---
+++ elecciones.js
@@ -0,0 +1,13 @@
+"use strict"
+
+const elecciones = [
+    {
+        opcion: "piedra"
+    },
+    {
+        opcion: "papel"
+    },
+    {
+        opcion: "tijera"
+    }
+]
