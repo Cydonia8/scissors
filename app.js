2f9adda5c31d710152f9197e03b9eb90cd5099dc
Álvaro Blanco Lucena <36717062+Cydonia8@users.noreply.github.com>
2022-11-15 21:12:55 +0100

    fin

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 "use strict"
+
+//Variables de control para el juego
 let ronda = 0;
 let puntos_jugador = 0;
 let nombre_maquina = "HAl 9000";
@@ -7,6 +9,7 @@ let opcion_jugador;
 let opcion_maquina;
 let nombre_jugador;
 
+//Constantes que recogen elementos relevantes para los eventos
 const array_opciones = ["piedra", "papel", "tijera"];
 const enviar = document.getElementById("enviar");
 const info_nombre_jugador = document.getElementById("nombre-jugador");
@@ -17,11 +20,9 @@ const total_jugador = document.getElementById("puntos-jugador");
 const total_maquina = document.getElementById("puntos-maquina");
 const nueva_partida = document.getElementById("nueva");
 const botones = document.querySelectorAll("button");
-console.log(botones);
-
-// const boton_
 
 
+//Evento para introducir el nombre del jugador
 enviar.addEventListener("click", ()=>{
     nombre_jugador = document.getElementById("input-usu").value;
     info_nombre_jugador.innerText=nombre_jugador;
@@ -31,6 +32,7 @@ enviar.addEventListener("click", ()=>{
 });
 
 
+//Evento para cada ronda del juego, ejecutado al pulsar el usuario sobre una opción
 cont_manos.addEventListener("click", (evento)=>{
     document.getElementById("enviar").setAttribute("disabled", true);
     const pulsado = evento.target.parentElement;
@@ -38,6 +40,7 @@ cont_manos.addEventListener("click", (evento)=>{
     opcion_maquina = array_opciones[Math.floor(Math.random() * array_opciones.length)];
     let ganador_ronda;
 
+    //Texto informativo sobre la elección de cada jugador
     resultado_ronda.previousElementSibling.innerText=`${nombre_jugador} escoge ${opcion_jugador}, ${nombre_maquina} escoge ${opcion_maquina}`;
 
     if(opcion_jugador == opcion_maquina){        
@@ -47,6 +50,8 @@ cont_manos.addEventListener("click", (evento)=>{
         resultado_ronda.innerText=`${ganador_ronda} gana la ronda`;
     }
     
+    //En función del ganador se actualizan sus puntos y se suma ronda. Las rondas sólo cuentan cuando
+    //alguno gana, no cuando hay empate
     if(ganador_ronda == nombre_jugador){
         puntos_jugador++;
         ronda++;
@@ -57,8 +62,11 @@ cont_manos.addEventListener("click", (evento)=>{
         total_maquina.innerText=puntos_maquina;
     }    
     
+    //Se actualiza el número de ronda en el texto 
     ronda_actual.innerText=`ronda ${ronda}`;
 
+    //Cuando se llega a 5 rondas, se determina quién ha ganado, se habilita el botón de nueva partida
+    //y desaparecen los botones
     if(ronda == 5){
         let ganador_juego = finJuego(puntos_jugador, puntos_maquina, nombre_jugador, nombre_maquina);
         resultado_ronda.nextElementSibling.innerText=`¡El ganador es ${ganador_juego}!`;
@@ -69,6 +77,10 @@ cont_manos.addEventListener("click", (evento)=>{
 
     }   
 })
+
+//Evento en el caso de que se seleccione el botón de nueva partida. Se reinician todas las variables de
+//control, incluidos los textos, desaparece el botón de nueva partida y se vuelven a mostrar los botones
+//de piedra, papel y tijera
 nueva_partida.addEventListener("click", ()=>{
     nueva_partida.style.display="none";
     ronda = 0;
@@ -85,6 +97,7 @@ nueva_partida.addEventListener("click", ()=>{
     })
 })
 
+//Funciona que determina cuándo termina el juego y quién gana
 function finJuego(puntos_jug, puntos_maq, nombre_jug, nombre_maq){
     let ganador;
         if(puntos_jug > puntos_maq){
@@ -95,6 +108,7 @@ function finJuego(puntos_jug, puntos_maq, nombre_jug, nombre_maq){
     return ganador;
 }
 
+//Funcion que, en caso de no haber empate, determina el ganador de la ronda
 function ganaRonda(opcion_maq, opcion_jug, nom_jug, nom_maq){
     let ganador_ronda;
     if((opcion_jug == "tijera" && opcion_maq == "papel") || (opcion_jug == "piedra" && opcion_maq == "tijera") || (opcion_jug == "papel" && opcion_maq == "piedra")){
